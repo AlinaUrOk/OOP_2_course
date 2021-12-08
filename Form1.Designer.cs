aa0fd8f86f866f59d1377f3d6082da3cbea80d52
Alina <71850840+AlinaUrOk@users.noreply.github.com>
2021-12-08 16:58:24 +0200

    add laba 2
    timer

---
+++ Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace laba_oop_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Timer = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SEC = new System.Windows.Forms.Label();
+            this.MIN = new System.Windows.Forms.Label();
+            this.HR = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 108);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(155, 46);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(289, 108);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(155, 46);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Timer
+            // 
+            this.Timer.AutoSize = true;
+            this.Timer.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Timer.Location = new System.Drawing.Point(160, 67);
+            this.Timer.Name = "Timer";
+            this.Timer.Size = new System.Drawing.Size(143, 38);
+            this.Timer.TabIndex = 12;
+            this.Timer.Text = "00:00:00";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // SEC
+            // 
+            this.SEC.AutoSize = true;
+            this.SEC.Location = new System.Drawing.Point(173, 123);
+            this.SEC.Name = "SEC";
+            this.SEC.Size = new System.Drawing.Size(24, 17);
+            this.SEC.TabIndex = 14;
+            this.SEC.Text = "00";
+            this.SEC.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // MIN
+            // 
+            this.MIN.AutoSize = true;
+            this.MIN.Location = new System.Drawing.Point(203, 123);
+            this.MIN.Name = "MIN";
+            this.MIN.Size = new System.Drawing.Size(24, 17);
+            this.MIN.TabIndex = 15;
+            this.MIN.Text = "00";
+            // 
+            // HR
+            // 
+            this.HR.AutoSize = true;
+            this.HR.Location = new System.Drawing.Point(243, 123);
+            this.HR.Name = "HR";
+            this.HR.Size = new System.Drawing.Size(24, 17);
+            this.HR.TabIndex = 16;
+            this.HR.Text = "00";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(172, 163);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 17;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(66, 163);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 18;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(278, 163);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 19;
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 218);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.HR);
+            this.Controls.Add(this.MIN);
+            this.Controls.Add(this.SEC);
+            this.Controls.Add(this.Timer);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Timer_Tick";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label Timer;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label SEC;
+        private System.Windows.Forms.Label MIN;
+        private System.Windows.Forms.Label HR;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace laba_oop_2
+{
+    public partial class Form1 : Form
+    {
+        int hours;
+        int minutes;
+        int seconds;
+        bool IsActive;
+
+        SoundPlayer sound = new SoundPlayer(Properties.Resources.xD);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IsActive = false;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            IsActive = true;
+
+            hours = Convert.ToInt32(textBox3.Text);
+            HR.Text = hours.ToString();
+
+            minutes = Convert.ToInt32(textBox1.Text);
+            MIN.Text = minutes.ToString();
+
+            seconds = Convert.ToInt32(textBox2.Text);
+            SEC.Text = seconds.ToString();
+
+            //bool b = int.TryParse(label1.Text, out seconds);
+            //if (b)
+            //{
+            //    SEC.Text = seconds.ToString();
+            //}
+
+            //timer1.Start();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            IsActive = false;
+            sound.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (IsActive)
+            {
+                if (hours == 0 && minutes == 0 && seconds == 0)
+                {
+                    sound.Play();
+                    timer1.Enabled = false;
+
+                }
+                if (seconds != 0 )
+                {
+                    seconds--;
+                    SEC.Text = seconds.ToString();
+                    if (seconds == 0)
+                    {
+                        if (minutes != 0)
+                        {
+                            minutes--;
+                            seconds = 59;
+                            SEC.Text = seconds.ToString();
+                            MIN.Text = minutes.ToString();
+                            if (minutes == 0)
+                            {
+                                if (hours != 0)
+                                {
+                                    hours--;
+                                    minutes = 59;
+                                    MIN.Text = minutes.ToString();
+                                    seconds = 59;
+                                    SEC.Text = seconds.ToString();
+                                    HR.Text = hours.ToString();
+                                }
+                            }
+                        }
+                    }
+                }
+                else if (minutes != 0 )
+                {
+                    minutes--;
+                    seconds = 59;
+                    SEC.Text = seconds.ToString();
+                    MIN.Text = minutes.ToString();
+                    if (minutes == 0)
+                    {
+                        if (hours != 0)
+                        {
+                            hours--;
+                            minutes = 59;
+                            MIN.Text = minutes.ToString();
+                            HR.Text = hours.ToString();
+                        }
+                    }
+                }
+                else if (hours != 0)
+                {
+                    hours--;
+                    minutes = 59;
+                    MIN.Text = minutes.ToString();
+                    seconds = 59;
+                    SEC.Text = seconds.ToString();
+                    HR.Text = hours.ToString();
+                }
+
+
+
+            }
+        }
+
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            {
+                if (!Char.IsDigit(e.KeyChar))
+                    e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            {
+                if (!Char.IsDigit(e.KeyChar))
+                    e.Handled = true;
+            }
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            {
+                if (!Char.IsDigit(e.KeyChar))
+                    e.Handled = true;
+            }
+        }
+    }
+}
