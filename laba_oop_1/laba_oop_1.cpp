8c42ee5e306fc7fcd2cd2c7b6991476557b2d2ed
Alina <71850840+AlinaUrOk@users.noreply.github.com>
2021-12-08 16:45:34 +0200

    add lab 1

---
+++ laba_oop_1/laba_oop_1.cpp
@@ -0,0 +1,435 @@
+﻿// laba_oop_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+/*  6.(**) Дерева з довільною кількістю дітей (на основі зв’язного списку та бібліотечних засобів для списку), бінарні дерева,
+		   дерева бінарного пошуку. Додавання елементів, видалення елементів (за батьківським вузлом та індексом, за значенням),
+		   пошук за значенням.
+*/
+
+
+
+
+
+
+#include <iostream>
+#include <stdlib.h>
+#include <list>
+using namespace std;
+
+typedef int T;
+
+typedef struct Node {
+	T data;
+	T index;
+	struct Node* left;
+	struct Node* right;
+	struct Node* parent;
+} Node;
+
+int Index;
+int Count = -1;
+list<int> List;
+
+Node* getFreeNode(T value, Node* parent) {
+	Node* tmp = (Node*)malloc(sizeof(Node));
+	tmp->left = tmp->right = NULL;
+	tmp->data = value;
+	tmp->parent = parent;
+	Index++;
+	List.push_back(value);
+	/*Count++;*/
+	tmp->index = Index;
+	return tmp;
+}
+
+void insert(Node** head, int value) {
+
+	Node* tmp = NULL;
+	Node* ins = NULL;
+	if (*head == NULL) {
+		Index = 0;
+		*head = getFreeNode(value, NULL);
+
+		return;
+	}
+
+	tmp = *head;
+	while (tmp) {
+		if (value > tmp->data) {
+			if (tmp->right) {
+				tmp = tmp->right;
+				continue;
+			}
+			else {
+				tmp->right = getFreeNode(value, tmp);
+				return;
+			}
+
+		}
+		else if (value < tmp->data) {
+			if (tmp->left) {
+				tmp = tmp->left;
+				continue;
+			}
+			else {
+				tmp->left = getFreeNode(value, tmp);
+				return;
+			}
+		}
+		else {
+			exit(2);
+		}
+	}
+}
+
+Node* findMinNode(Node* root) {
+	while (root->left) {
+		root = root->left;
+	}
+	return root;
+}
+
+Node* findMaxNode(Node* root) {
+	while (root->right) {
+		root = root->right;
+	}
+	return root;
+}
+
+Node* getNodeByValue(Node* root, T value) {
+	while (root) {
+		if (root->data > value) {
+			root = root->left;
+			continue;
+		}
+		else if (root->data < value) {
+			root = root->right;
+			continue;
+		}
+		else if (root->data == value)
+		{
+			cout << value << " Complete!" << endl;
+			return root;
+		}
+	}
+	cout << value << " Error..." << endl;;
+	return NULL;
+}
+
+
+
+//template <class T>
+//void Depth(Node* t)
+//{
+//	int depthLeft, depthRight, depthval;
+//
+//	if (t == NULL)
+//		depthval = -1;
+//	else
+//	{
+//		depthLeft = Depth(t->Left());
+//		depthRight = Depth(t->Right());
+//		depthval = 1 + (depthLeft > depthRight , depthLeft : depthRight);
+//		cout << t->data << " depth- " << depthval << endl;
+//	}
+//	return depthval;
+//}
+
+void removeNodeByPtr(Node* target) {
+	if (target->left && target->right) {
+		Node* localMax = findMaxNode(target->left);
+		target->data = localMax->data;
+		removeNodeByPtr(localMax);
+		return;
+	}
+	else if (target->left) {
+		if (target == target->parent->left) {
+			target->parent->left = target->left;
+			/*target->parent->left->index = target->parent->left->index - 1;*/
+			if (target->right)
+			{
+				target->right->index--;
+			}
+			if (target->left)
+			{
+				target->left->index--;
+			}
+		}
+		else {
+			target->parent->right = target->left;
+			/*target->parent->right->index = target->parent->right->index - 1;*/
+			if (target->right)
+			{
+				target->right->index--;
+			}
+			if (target->left)
+			{
+				target->left->index--;
+			}
+		}
+	}
+	else if (target->right) {
+		if (target == target->parent->right) {
+			target->parent->right = target->right;
+			/*target->parent->right->index = target->parent->right->index - 1;*/
+			if (target->right)
+			{
+				target->right->index--;
+			}
+			if (target->left)
+			{
+				target->left->index--;
+			}
+		}
+		else {
+			target->parent->left = target->right;
+			/*target->parent->left->index = target->parent->left->index - 1;*/
+			if (target->right)
+			{
+				target->right->index--;
+			}
+			if (target->left)
+			{
+				target->left->index--;
+			}
+		}
+	}
+	else {
+		if (target == target->parent->left) {
+			target->parent->left = NULL;
+		}
+		else {
+			target->parent->right = NULL;
+		}
+	}
+
+	free(target);
+}
+
+//void removeNodeByIndex(Node* target) {
+//	if (target->left && target->right) {
+//		Node* localMax = findMaxNode(target->left);
+//		target->data = localMax->data;
+//		removeNodeByIndex(localMax);
+//		return;
+//	}
+//	else if (target->left) {
+//		if (target == target->parent->left) {
+//			target->parent->left = target->left;
+//		}
+//		else {
+//			target->parent->right = target->left;
+//		}
+//	}
+//	else if (target->right) {
+//		if (target == target->parent->right) {
+//			target->parent->right = target->right;
+//		}
+//		else {
+//			target->parent->left = target->right;
+//		}
+//	}
+//	else {
+//		if (target == target->parent->left) {
+//			target->parent->left = NULL;
+//		}
+//		else {
+//			target->parent->right = NULL;
+//		}
+//	}
+//
+//	free(target);
+//}
+
+
+
+void deleteValue(Node* root, T value) {
+	Node* target = getNodeByValue(root, value);
+	removeNodeByPtr(target);
+}
+
+
+//void deleteIndex(Node* root, T value) {
+//	Node* target = Circle(root, value);
+//	removeNodeByIndex(target);
+//}
+
+
+void printTree(Node* root, const char* dir, int level) {
+	if (root) {
+		cout << "level " << level << " " << dir << " = " << root->data << " || " << root->index << endl;
+		printTree(root->left, "left", level + 1);
+		printTree(root->right, "right", level + 1);
+	}
+}
+
+void inorder(Node* root)
+{
+	if (root == NULL)
+	{
+		return;
+	}
+	inorder(root->left);
+	cout << root->data << " ";
+	inorder(root->right);
+}
+
+int main() {
+	Node* root = NULL;
+	insert(&root, 10);
+	insert(&root, 12);
+	insert(&root, 8);
+	insert(&root, 9);
+	insert(&root, 7);
+	insert(&root, 3);
+
+	printTree(root, "root", 0);
+	cout << "----------------------------------------" << endl;
+	deleteValue(root, 7);
+	printTree(root, "root", 0);
+	cout << "----------------------------------------" << endl;
+	/*getNodeByIndex(root, 2);*/
+	//printTree(root, "root", 0);
+	//cout << "----------------------------------------" << endl;
+	//deleteIndex(root, 3);
+	//printTree(root, "root", 0);
+	//cout << "----------------------------------------" << endl;
+	getNodeByValue(root, 3);
+	cout << "----------------------------------------" << endl;
+	getNodeByValue(root, 5);
+	cout << "----------------------------------------" << endl;
+	/*Depth<int>(root);*/
+	inorder(root);
+}
+
+
+//
+//struct Data
+//{
+//	int data;
+//	Data* left;
+//	Data* right;
+//	Data* parent;
+//};
+//
+//class List
+//
+//{
+//	List* head;
+//public:
+//	Data* Add(int data)
+//	{
+//		Data* temp = new Data;
+//		temp->data = data;
+//		temp->left = NULL;
+//		temp->right = NULL;
+//		temp->parent = NULL;
+//		return temp;
+//	}
+//
+//	Data* push_back(Data* node, int key)
+//	{
+//		if (node == NULL)
+//			return Add(key);
+//		if (key < node->data) {
+//			node->left = push_back(node->left, key);
+//			node->left->parent = node;
+//		}
+//		else if (key > node->data) {
+//			node->right = push_back(node->right, key);
+//			node->right->parent = node;
+//		}
+//		return node;
+//	}
+//
+//	Data* print(Data* node) {
+//		if (node == NULL) {
+//			return;
+//		}
+//		print(node->left);
+//		if (node->parent != NULL && node->parent->data % 2) {
+//			std::cout << node->data << " => " << node->parent->data << "\n";
+//		}
+//		print(node->right);
+//	}
+//
+//};
+//
+//int main()
+//{
+//	Data* root = NULL;
+//	std::list <int> lst;
+//	lst.push_back(27);
+//	lst.push_back(5);
+//	lst.push_back(100);
+//	lst.push_back(4);
+//	lst.push_back(1);
+//
+//	lst.print(root);
+//}
+
+//template<typename T>
+//void List<T>::push_back(T data) {
+//	if (head == nullptr) {
+//		head = new Node<T>(data);
+//	}
+//	else {
+//		Node<T>* current = this->head;
+//		while (current->pNext != nullptr) {
+//			current = current->pNext;
+//		}
+//		current->pNext = new Node<T>(data);
+//	}
+//	Size++;
+//}
+
+//class Tree
+//{
+//public:
+//	int data;
+//	Tree* left=NULL, * right=NULL;
+//
+//	Tree* AddNode(Tree* tree, int data)
+//	{
+//		if (!tree)
+//		{
+//			tree = new Tree;
+//			tree->data = data;
+//			tree->left = NULL;
+//			tree->right = NULL;
+//		}
+//		if (data > tree->data)
+//		{
+//			AddNode(tree->left, data);
+//		}
+//		if (data < tree->data)
+//		{
+//			AddNode(tree->right, data);
+//		}
+//		return tree;
+//	};
+//
+//	void Print(Tree* tree)
+//	{
+//		if(tree != NULL)
+//		{
+//			Print(tree->left);
+//			std::cout << tree->data;
+//			Print(tree->right);
+//		}
+//	}
+//
+//};
+//
+// int main()
+//{
+//	 Tree tree, *root=NULL;
+//	 tree.AddNode(root, 2);
+//	 tree.AddNode(root, 5);
+//	 tree.AddNode(root, 3);
+//	 tree.AddNode(root, 7);
+//	 tree.AddNode(root, 9);
+//
+//	 tree.Print(root);
+//	 return 0;
+//}
---
+++ laba_oop_2.cpp
@@ -0,0 +1,222 @@
+﻿// laba_oop_2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+/*Інформація про дату (рік, місяць, день) та час (години, хвилини, секунди). Перевірка правильності дати та часу відповідно до григоріанського календаря.
+Арифметика моментів часу: різниця між двома моментами часу у заданих одиницях, додавання чи віднімання такої різниці до заданого моменту часу.
+
+Обчислення дня тижня для заданої дати.*/
+
+#include <iostream>
+
+class Date {
+private:
+	bool isLeapYear;
+	int maxDay()const;
+	bool isLeap(int)const;
+	int countDayYear;
+
+	void dayOfTheWeek();
+	void counterOfDaysAtYear();
+	int indexTheDayOfWeek;
+	void createNewDataBySec(long long current_sec);
+public:
+	int year;
+	int month;
+	int day;
+	int hour;
+	int min;
+	int sec;
+	Date(int year, int month, int day, int hour, int min, int sec);
+	Date(int year, int day, int month);
+	Date() = default;
+	void DifofTwoDates(Date& first_date, Date& second_date);
+	void SumofTwoDates(Date& first_date, Date& second_date);
+	long long countSecForData()const;
+	void Print()
+	{
+		std::cout << "Your data: " << year << ", " << month << ", " << day << "   " << hour << ":" << min << ":" << sec << " "; std::cout << std::endl;
+	}
+};
+
+Date::Date(int year, int month, int day, int hour, int min, int sec) {
+	isLeapYear = isLeap(year);
+	this->year = year;
+
+	if (month > 12 || month <= 0) {
+		std::cout << "Incorrect month" << std::endl;
+		this->month = 1;
+	}
+	else
+		this->month = month;
+
+	if (day > maxDay()) {
+		std::cout << "Incorrect day" << std::endl;
+		this->day = 1;
+	}
+	else
+		this->day = day;
+
+	if (hour > 24 || hour < 0) {
+		std::cout << "Incorrect hour " << std::endl;
+		this->hour = 1;
+	}
+	else
+		this->hour = hour;
+	if (min > 60 || min < 00) {
+		std::cout << "Incorrent minutes" << std::endl;
+		this->min = 1;
+	}
+	else
+		this->min = min;
+	if (sec > 60 || sec < 0) {
+		std::cout << "Incorrect sec" << std::endl;
+		this->sec = 1;
+	}
+	else
+		this->sec = sec;
+
+
+}
+
+Date::Date(int year, int month, int day) {
+	isLeapYear = isLeap(year);
+
+	this->year = year;
+
+	if (month > 12 || month <= 0) {
+		std::cout << "Incorrect month" << std::endl;;
+		this->month = 1;
+	}
+	else
+		this->month = month;
+
+	if (day > maxDay()) {
+		std::cout << "Incorrect day" << std::endl;
+		this->day = 1;
+	}
+	else
+		this->day = day;
+
+	hour = min = sec = 1;
+
+
+}
+
+bool Date::isLeap(int y)const {
+	return ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0);
+}
+
+int Date::maxDay() const {
+	int days[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+	if (isLeapYear)
+		days[1] = 29;
+	return days[month - 1];
+}
+
+
+//преобразование даты в секунды
+long long Date::countSecForData()const {
+	long long data = day * 86400 + hour * 3600 + min * 60 + sec;
+	for (int i = 1; i < year; i++) {
+		if ((i % 4 == 0 && i % 100 != 0) || i % 400 == 0)
+			data += 366 * 86400;
+		else
+			data += 365 * 86400;
+
+	}
+	std::cout << "Date in second: " << data; std::cout << std::endl;
+	return data;
+}
+
+void Date::DifofTwoDates(Date& first_date, Date& second_date)
+{
+	long long date_one = first_date.countSecForData();
+	long long date_two = second_date.countSecForData();
+	long long dif_date;
+	if (date_one > date_two)
+	{
+		dif_date = date_one - date_two;
+	}
+	else
+	{
+		dif_date = date_two - date_one;
+	}
+	std::cout << "Date in second: " << dif_date; std::cout << std::endl;
+	second_date.createNewDataBySec(dif_date);
+}
+
+void Date::SumofTwoDates(Date& first_date, Date& second_date)
+{
+	long long date_one = first_date.countSecForData();
+	long long date_two = second_date.countSecForData();
+	long long sum_date;
+	sum_date = date_one + date_two;
+
+
+	std::cout << "Sum in second: " << sum_date; std::cout << std::endl;
+	second_date.createNewDataBySec(sum_date);
+}
+
+void Date::createNewDataBySec(long long current_sec) {
+	year = 0;
+	while (current_sec >= 365 * 24 * 60 * 60) {
+		if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+			if (current_sec >= 366 * 24 * 60 * 60) {
+				current_sec -= 366 * 24 * 60 * 60;
+			}
+		}
+		else {
+			current_sec -= 365 * 24 * 60 * 60;
+		}
+		year++;
+	}
+	isLeapYear = isLeap(year);
+
+	int counter_days = current_sec % 60 % 60 % 24;
+	day = counter_days;
+
+	sec = current_sec % 60;
+	current_sec %= 60;
+	min = current_sec % 60;
+	current_sec %= 60;
+	hour = current_sec % 24;
+	current_sec %= 24;
+	month = current_sec % 12;
+
+}
+
+int main()
+{
+	int year, month, day, hour, min, sec;
+	std::cout << "Enter date"; std::cout << std::endl; std::cout << "Year: ";
+	std::cin >> year; std::cout << "Month: ";
+	std::cin >> month; std::cout << "Day: ";
+	std::cin >> day;  std::cout << "Hour: ";
+	std::cin >> hour;  std::cout << "Minute: ";
+	std::cin >> min;  std::cout << "Second: ";
+	std::cin >> sec;
+	Date date(year, month, day, hour, min, sec);
+	date.Print();
+	/*date.countSecForData();*/
+	std::cout << "_____________________________________________________________" << std::endl;
+	int year1, month1, day1, hour1, min1, sec1;
+	std::cout << "Enter second date"; std::cout << std::endl; std::cout << "Year: ";
+	std::cin >> year1; std::cout << "Month: ";
+	std::cin >> month1; std::cout << "Day: ";
+	std::cin >> day1;  std::cout << "Hour: ";
+	std::cin >> hour1;  std::cout << "Minute: ";
+	std::cin >> min1;  std::cout << "Second: ";
+	std::cin >> sec1;
+	Date date1(year1, month1, day1, hour1, min1, sec1);
+	date1.Print();
+	//date1.countSecForData();
+	date1.DifofTwoDates(date, date1);
+	date1.Print();
+	std::cout << "_____________________________________________________________" << std::endl;
+	date1.DifofTwoDates(date, date1);
+	date1.Print();
+
+
+	/*std::cout << "_____________________________________________________________" << std::endl;
+	date1.SumofTwoDates(date, date1);
+	date1.Print();*/
+}
